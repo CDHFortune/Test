2ef8fd99500f57c8c4256ba98f450acbc250909e
CDHFortune <cdhfortune@163.com>
2019-10-31 15:33:56 +0800

    测试文件

--- demo1/src/action/LoginAction.java
+++ demo1/src/action/LoginAction.java
@@ -12,7 +12,7 @@ public class LoginAction extends ActionSupport {
 	public InputStream getInputStream() {
 		return inputStream;
 	}
-	
+	//测试文件
 	public void setInputStream(InputStream inputStream) {
 		this.inputStream = inputStream;
 	}
